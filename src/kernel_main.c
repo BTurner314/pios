ce53d47554e5ab6e3c7e6dbd960ea00aa1c108c0
BTurner314 <bturner3@luc.edu>
2021-02-09 14:48:20 -0600

    homework 1

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,8 +1,17 @@
 
-
+#include <stdio.h>
 
 void kernel_main() {
 
-    while(1){
-    }
+    char *begin_bss = &__bss_start;
+    char *end_bss = &__bss_end;
+
+    size_t i;
+    for (i = 0; i < begin_bss; ++i){
+	begin_bss[i] = 0;
+	}
+return 0;
+
 }
+
+
---
+++ src/list.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main {
+}
+
+struct list_element {
+	struct list_element *next;
+	int data;
+}
